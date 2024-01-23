f8682d89e1b35391c8bac6571888054b3e5e4b7c
unknown <DT020018@vdiscovery.cn>
2024-01-23 18:19:06 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+const name = ref('cici')
+</script>
+<template>
+  <div>hello {{ name }}, this is helloworld components</div>
+  <button class="btn-style">Button</button>
+</template>
+<style scoped>
+.btn-style {
+  @apply bg-blue-400 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200;
+  @apply hover:bg-blue-500;
+  @apply dark:bg-blue-500 dark:hover:bg-blue-600;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<script setup>
+const handleClickMini = () => {
+  AMessage.info('hello cici, click mini AButton!')
+}
+</script>
+
+<template>
+  <div>hello vue3+ts, This is home page!</div>
+  <a-space>
+    <a-button type="primary" size="mini" @click="handleClickMini"
+      >Mini</a-button
+    >
+    <a-button type="primary" size="small">Small</a-button>
+    <a-button type="primary">Medium</a-button>
+    <a-button type="primary" size="large">Large</a-button>
+  </a-space>
+  <HelloWorld />
+</template>
+
+<style scoped></style>
